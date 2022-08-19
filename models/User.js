4925499b2c08b4a99ecf8e74f4d15f18ee05ac7e
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-18 22:12:46 -0500

    create user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const {Schema, model} = require('mongoose');
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: [true, 'A username is required'],
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: [true, 'An email is required'],
+            unique: true,
+            match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/gi
+        }, 
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
