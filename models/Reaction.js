59f363fadc322b6d71b4f7be676f98e5a4809e2a
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-18 23:26:26 -0500

    create reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,9 +1,31 @@
-
 const {Schema, model} = require('mongoose');
 
 const reactionSchema = new Schema(
     {
-
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: [true, 'You must have an actual reaction'],
+            max: [280, 'Exceeded maximum character limit of 280']
+        },
+        username: {
+            type: String,
+            required: [true, 'There must be an originating user']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: new Date().toLocaleDateString()
+        }
+    }, 
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
     }
 )
 
